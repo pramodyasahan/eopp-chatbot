b8bfea637be5723d9b128fab78d7037116736ccb
pramodyasahan <pramodyasahan.edu@gmail.com>
2025-02-06 23:51:07 +0530

    Fix the error of the eopp tool

--- chatbot/eopp_tool.py
+++ chatbot/eopp_tool.py
@@ -2,90 +2,32 @@ import json
 from langchain.tools import tool
 from chatbot.filter import initial_filtering
 
-# Define allowed categories for filtering
-ALLOWED_UNIVERSITIES = {
-    "university college birmingham",
-    "university of westminster",
-    "university of suffolk",
-    "university of west london",
-    "university of worcester"
-}
-
-ALLOWED_FIELD_TYPES = {
-    "healthcare",
-    "computer science",
-    "business",
-    "engineering",
-    "law",
-    "Arts & Humanities",
-    "Education",
-    "Social Sciences",
-    "Agriculture & Environmental Science",
-    "Natural Sciences"
-}
-
-ALLOWED_LOCATIONS = {
-    "birmingham",
-    "ipswich",
-    "westminster",
-    "suffolk",
-    "london",
-    "central london"
-}
-
-ALLOWED_DEGREE_TYPES = {
-    "bachelor's",
-    "master's",
-    "foundation",
-    "phd",
-    "level 1",
-    "level 2",
-    "level 3",
-    "a level"
-}
-
 
 @tool()
 def initial_filtering_tool(filters_json: str) -> str:
     """
     Initial Filtering Tool
 
-    This tool filters **courses based on user input criteria** OR returns **universities offering a specific course**.
+    Filters courses based on user criteria or returns universities offering a specific course.
 
-    ---
-    Allowed Filtering Criteria:
-    - University Name: MUST be one of the following:
-      {allowed_universities}
-    - Field Type: MUST be one of the following:
-      {allowed_field_types}
-    - Degree Level: MUST be one of the following:
-      {allowed_degree_types}
-    - Location: MUST be one of the following:
-      {allowed_locations}
-    - Course Name: If the user asks for a specific course, the tool will return the universities that offer it.
+    Allowed Filters:
+    - University Name: university college birmingham, university of westminster, university of suffolk, university of west london, university of worcester.
+    - Field Type: healthcare, computer science, business, engineering, law, Arts & Humanities, Education, Social Sciences, Agriculture & Environmental Science, Natural Sciences.
+    - Degree Level: bachelor's, master's, foundation, phd.
+    - Location: birmingham, ipswich, westminster, suffolk, london.
+    - Course Name: Returns universities offering the specified course.
 
-    ---
-    Example Usage:
-    ```
+    Usage Example:
     {
-        "university name": "university of westminster",
-        "field type": "computer science",
+        "university name": null,
+        "field type": "engineering",
         "location": "london",
-        "degree program type": "master's"
+        "degree program type": "bachelor's"
     }
-    ```
 
-    OR to find the university that offers a specific course:
-    ```
-    {
-        "course name" -> "university name"
-    }
-    ```
-
-    Response: A comma-separated list of matching courses or universities.
-
-    ---
+    Response: A comma-separated list of matching courses/universities or an appropriate message.
     """
+
     try:
         filters = json.loads(filters_json)
         print("Filters received:", filters)
